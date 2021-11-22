b1ae98ce83e0bb8b31af01a4127dde6c16d1eb5c
Muhammad Tashfiqul Bari <tashfiqulbari@gmail.com>
2021-11-22 10:41:37 +0600

    Testing Commit

---
+++ Assets/==Project==/===Module===/GridBuilder/Runtime/Scripts/GridBuilderManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Project.Shared;
+
+public class GridBuilderManager : GameBehaviour
+{
+    #region ALL UNITY FUNCTIONS
+
+    #endregion ALL UNITY FUNCTIONS
+
+    //=================================   
+    #region ALL OVERRIDING FUNCTIONS
+    
+    #endregion ALL OVERRIDING FUNCTIONS
+
+    //=================================
+    #region ALL SELF DECLEAR FUNCTIONS
+    
+    #endregion ALL SELF DECLEAR FUNCTIONS
+
+}

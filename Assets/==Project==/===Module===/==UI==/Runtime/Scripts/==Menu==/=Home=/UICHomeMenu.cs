2891b27ba56bce1d14522dd84b36584c7befe813
Muhammad Tashfiqul Bari <tashfiqulbari@gmail.com>
2021-11-23 10:45:27 +0600

    BasicUI Implementation is done

--- Assets/==Project==/===Module===/==UI==/Runtime/Scripts/==Menu==/=Home=/UICHomeMenu.cs
+++ Assets/==Project==/===Module===/==UI==/Runtime/Scripts/==Menu==/=Home=/UICHomeMenu.cs
@@ -1,9 +1,31 @@
 namespace Project.UI
 {
     using UnityEngine;
+    using UnityEngine.UI;
 
     public class UICHomeMenu : UICanvas
     {
+        #region Private Variables
+
+        [SerializeField] private Button _startButton;
+
+        #endregion
+
+        #region Mono Behaviour
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            _startButton.onClick.AddListener(() =>{
+
+                SetCanvasVisibility(false);
+                _gameManager.OnLevelStartedEvent.Raise();
+            });
+        }
+
+        #endregion
+
         #region Override Method
 
         protected override void OnCanvasEnabled()
